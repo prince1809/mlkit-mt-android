ad57dcbb748b76a725a2a7ba2133a400c9dfb94b
prince <prince18pat@gmail.com>
2019-07-16 00:21:12 +0900

    update gradle and update project to andoridx

--- app/src/main/java/com/princekr/android/ml/md/MainActivity.java
+++ app/src/main/java/com/princekr/android/ml/md/MainActivity.java
@@ -1,13 +1,17 @@
 package com.princekr.android.ml.md;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.activity_main);
     }
 }
